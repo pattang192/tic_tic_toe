446c88b24194e58c9dbfa611c5193ad7302b5eac
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-03-22 18:31:55 -0400

    Write method that gets the spot the player wants to mark

--- main.rb
+++ main.rb
@@ -19,3 +19,10 @@ puts "\nIt's time for a game of Tic Tac Toe!"
 puts pre_game_board
 
 puts "\nPlayer 1 is X \nPlayer 2 is O"
+
+module PlayGame
+  def get_play_position()
+    puts "#{self.name}, choose your position:"
+    play_position = gets.chomp.to_i
+  end
+end
