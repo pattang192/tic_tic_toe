49500741bda2bd6aa1981ddf6e416a7ef30a6ea4
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-03-25 12:15:29 -0400

    Include exception handling in get play position method

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,22 @@
 # player.rb
 
 module PlayGame
-  def get_play_position()
+  def get_play_position(game_board)
+
+    boxes = [game_board[21], game_board[27], game_board[33], game_board[75],
+    game_board[81], game_board[87], game_board[129], game_board[135], game_board[141]]
+
       puts "#{self.name}, choose your position:"
       play_position = gets.chomp.to_i
-  end
+
+      until play_position.between?(1, 9) && boxes[play_position-1] == " "
+        puts "Invalid input. #{self.name}, choose your position:"
+        play_position = gets.chomp.to_i
+      end
+
+      play_position
+
+    end
   end
 
 class Player
