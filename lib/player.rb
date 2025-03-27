932b34039d1828f0922994cec932fba820f2ba66
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-03-27 11:54:41 -0400

    Refactor methods that get and set play position

--- lib/player.rb
+++ lib/player.rb
@@ -1,44 +1,21 @@
 # player.rb
 
 module PlayGame
-  def get_play_position(game_board)
-
-    boxes = [game_board[21], game_board[27], game_board[33], game_board[75],
-    game_board[81], game_board[87], game_board[129], game_board[135], game_board[141]]
-
+  def get_play_position(game_board, boxes)
       puts "#{self.name}, choose your position:"
-      play_position = gets.chomp.to_i
+      play_position = gets.chomp.to_i - 1
 
-      until play_position.between?(1, 9) && boxes[play_position-1] == " "
+      until play_position.between?(0, 8) && boxes[play_position] == " "
         puts "Invalid input. #{self.name}, choose your position:"
-        play_position = gets.chomp.to_i
+        play_position = gets.chomp.to_i - 1
       end
-
       play_position
-
     end
 
   def set_play_position(play_position, symbol, game_board)
-    case play_position
-    when 1
-      game_board[21] = symbol
-    when 2
-      game_board[27] = symbol
-    when 3
-      game_board[33] = symbol
-    when 4
-      game_board[75] = symbol
-    when 5
-      game_board[81] = symbol
-    when 6
-      game_board[87] = symbol
-    when 7
-      game_board[129] = symbol
-    when 8
-      game_board[135] = symbol
-    when 9
-      game_board[141] = symbol
-    end
+    box = [21, 27, 33, 75, 81, 87, 129, 135, 141] # indices game board's playable spots
+    game_board[box[play_position]] = symbol
+
     puts game_board
   end
 
