ffa6abe82fcf69286a02b362056f9c61aa1c69d6
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-02 10:58:45 -0400

    Refactor main methods

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require "rubocop"
 require_relative "lib/player"
+require_relative "lib/tic_tac_toe"
 
 board = "
 |     |     |     |
@@ -21,40 +22,43 @@ puts board
 
 puts "\nPlayer 1 is X \nPlayer 2 is O"
 
+board_index = ["filler"]
+board.chars.each_with_index do |char, i|
+  board_index.push(i) if %w[1 2 3 4 5 6 7 8 9].include?(char)
+end
+
 def clear_board(board)
   board.gsub!(/[123456789XO]/, " ")
 end
 
 clear_board(board)
 
-box = [21, 27, 33, 75, 81, 87, 129, 135, 141] # indices of board's playable spots
-
 player1 = Player.new("Player 1", "X")
 player2 = Player.new("Player 2", "O")
 
 players = [player1, player2]
 
-def play_game(players, board, box)
+def play_game(board, board_index, players)
   loop do
     players.each do |player|
-      play_position = player.get_play_position(board, box)
-      player.set_play_position(play_position, player.symbol, board, box)
-      if player.game_over?(board, player.symbol)
-        player.declare_winner(board, player.symbol)
-        end_game(board, players, box)
+      play_position = player.get_play_position(board, board_index)
+      player.set_play_position(board, board_index, play_position)
+      if player.game_over?(board, board_index)
+        player.declare_winner(board, board_index)
+        end_game(board, board_index, players)
       end
     end
   end
 end
 
-def end_game(board, players, box)
+def end_game(board, board_index, players)
   clear_board(board)
   puts "Play again? y/n"
   play_again = gets.chomp
-  if play_again == "y" then play_game(players, board, box)
+  if play_again == "y" then play_game(board, board_index, players)
   else
     exit
   end
 end
 
-play_game(players, board, box)
+play_game(board, board_index, players)
