f9e169612d1200efd7611c7c9ff35b5cb090ec5f
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2025-04-02 12:58:54 -0400

    Refactor check for a winner

--- main.rb
+++ main.rb
@@ -33,32 +33,36 @@ end
 
 clear_board(board)
 
+three_in_a_line = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                   [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                   [1, 5, 9], [3, 5, 7]]
+
 player1 = Player.new("Player 1", "X")
 player2 = Player.new("Player 2", "O")
 
 players = [player1, player2]
 
-def play_game(board, board_index, players)
+def play_game(board, board_index, players, three_in_a_line)
   loop do
     players.each do |player|
       play_position = player.get_play_position(board, board_index)
       player.set_play_position(board, board_index, play_position)
-      if player.game_over?(board, board_index)
-        player.declare_winner(board, board_index)
-        end_game(board, board_index, players)
+      if player.game_over?(board, board_index, three_in_a_line)
+        player.declare_winner(board, board_index, three_in_a_line)
+        end_game(board, board_index, players, three_in_a_line)
       end
     end
   end
 end
 
-def end_game(board, board_index, players)
+def end_game(board, board_index, players, three_in_a_line)
   clear_board(board)
   puts "Play again? y/n"
   play_again = gets.chomp
-  if play_again == "y" then play_game(board, board_index, players)
+  if play_again == "y" then play_game(board, board_index, players, three_in_a_line)
   else
     exit
   end
 end
 
-play_game(board, board_index, players)
+play_game(board, board_index, players, three_in_a_line)
